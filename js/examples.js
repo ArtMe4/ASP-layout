15ddddbce358b7cfb627f719e876a1f0277de4df
isakov-dev <isakov.developer@gmail.com>
2021-03-26 13:53:07 +0600

    Added examples sliders

---
+++ js/examples.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    new Swiper('.examples__slider', {
+
+        spaceBetween: 10,
+        slidesPerView: 'auto',
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+    });
+
+
+});
