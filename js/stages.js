a40476933d2bf2256ae6ac74351b2a2fd6656732
isakov-dev <isakov.developer@gmail.com>
2021-03-29 14:23:09 +0600

    Completed stages with adaptive & animations

--- js/stages.js
+++ js/stages.js
@@ -1,5 +1,67 @@
+jQuery.fn.reverse = [].reverse;
+
 $(document).ready(function () {
 
-    $('.stages__row').addClass('stages__row_active');
+    startStagesAnimation();
 
 });
+
+function startStagesAnimation () {
+
+    let lineTimeOut = 0;
+    let itemTimeout = 0;
+
+    $('.stages__row').each(function (index) {
+
+        let self = this;
+
+        setTimeout(function () {
+            $(self).addClass('stages__row_active');
+        }, lineTimeOut);
+
+        lineTimeOut += 700;
+
+        setTimeout(function () {
+            $(self).addClass('stages__row_active-vertical');
+        }, lineTimeOut);
+
+        lineTimeOut += 350;
+
+        let $items = $(self).find('.stages__item');
+
+        if (index % 2 === 0) {
+
+            $items.each(function () {
+
+                let self = this;
+
+                setTimeout(function () {
+                    $(self).addClass('stages__item_active');
+                }, itemTimeout);
+
+                itemTimeout += 350;
+
+            });
+
+        } else {
+
+            $items.reverse().each(function () {
+
+                let self = this;
+
+                setTimeout(function () {
+                    $(self).addClass('stages__item_active');
+                }, itemTimeout);
+
+                itemTimeout += 350;
+
+            });
+
+        }
+
+        if (window.innerWidth > 767)
+            itemTimeout += 350;
+
+    });
+
+}
