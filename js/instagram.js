5b6cb9cd287df47c7411563aca0d4f877bfa637b
isakov-dev <isakov.developer@gmail.com>
2021-04-08 13:46:50 +0600

    Added animation for instagram block

--- js/instagram.js
+++ js/instagram.js
@@ -7,7 +7,20 @@ $(document).ready(function () {
             992: {
                 spaceBetween: 15,
             },
-        }
+        },
+        on: {
+            init: function () {
+                //animation
+                let timeout = 0;
+                $('.instagram__photo').each(function () {
+                    let self = this;
+                    setTimeout(function () {
+                        $(self).addClass('instagram__photo_active');
+                    }, timeout);
+                    timeout += 75;
+                });
+            },
+        },
     });
 
 });
