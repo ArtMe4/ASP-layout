981b457b4c944a87de18317052f6edd558182228
Artur Meshchenkov <artur133715@gmail.com>
2021-06-24 15:32:15 +0600

    feat: тестовые данные для календаря(пуш для дома)

--- js/calendar.js
+++ js/calendar.js
@@ -3,25 +3,51 @@ $(document).ready(function () {
     let calendar = new Vue({
         el: '#calendarApp',
         data: {
-            year: 2021,
-            month: "Март",
-            month_genitive: "Марта",
+
             firstDayOffset: 2,
             daysInMonth: 31,
+
+            eventsMonths: [
+
+                {
+                    month: "Март",
+                    month_genitive: "Марта",
+                    monthNumber: 5,
+                },
+                {
+                    month: "Июнь",
+                    month_genitive: "Июня",
+                    monthNumber: 6,
+                },
+                {
+                    month: "Июль",
+                    month_genitive: "Июля",
+                    monthNumber: 7,
+                }
+
+            ],
+
             events: [
                 {
+                    year: 2021,
+                    month: "Март",
+                    month_genitive: "Марта",
                     day: 3,
                     type: "Online-курс",
                     name: "Свадебный наряд моей дочери",
                     link: "#"
                 },
                 {
+                    month: "Июнь",
+                    month_genitive: "Июня",
                     day: 11,
                     type: "Оффлайн-курс",
                     name: "Свадебный наряд для попугая",
                     link: "#"
                 },
                 {
+                    month: "Июль",
+                    month_genitive: "Июля",
                     day: 24,
                     type: "Online-курс",
                     name: "Свадебный наряд для пингвина",
@@ -32,6 +58,13 @@ $(document).ready(function () {
             selectedDay: null,
         },
         methods: {
+            getMonths: function () {
+
+                let arrayOfMonths = this.eventsMonths.find(function (event) {
+                    console.log(event.monthNumber);
+                });
+
+            },
             getEventByDay: function (day) {
                 return this.events.find(function (event) {
                     return event.day == day;
