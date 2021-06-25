975a1dfcde8ffe2a34282ba2026f8cdc447ae0d0
Artur Meshchenkov <artur133715@gmail.com>
2021-06-25 18:20:55 +0600

    feat: пуш для дома

--- js/calendar.js
+++ js/calendar.js
@@ -3,52 +3,75 @@ $(document).ready(function () {
     let calendar = new Vue({
         el: '#calendarApp',
         data: {
-
-            firstDayOffset: 2,
-            daysInMonth: 31,
-
-            eventsMonths: [
-
+            // year: 2021,
+            // month: "Июль",
+            // month_genitive: "Июля",
+            // monthNumber: 7,
+            // firstDayOffset: 2,
+            // daysInMonth: 31,
+            months: [
                 {
-                    month: "Март",
-                    month_genitive: "Марта",
+                    month: "Май",
+                    month_genitive: "Мая",
                     monthNumber: 5,
+                    year: 2021,
+                    daysInMonth: 30,
+                    firstDayOffset: 1,
                 },
                 {
                     month: "Июнь",
                     month_genitive: "Июня",
                     monthNumber: 6,
+                    year: 2021,
+                    daysInMonth: 31,
+                    firstDayOffset: 2,
                 },
                 {
                     month: "Июль",
                     month_genitive: "Июля",
                     monthNumber: 7,
+                    year: 2021,
+                    daysInMonth: 30,
+                    firstDayOffset: 3,
+                },
+                {
+                    month: "Июль",
+                    month_genitive: "Июля",
+                    monthNumber: 7,
+                    year: 2021,
+                    daysInMonth: 30,
+                    firstDayOffset: 3,
                 }
-
             ],
-
             events: [
                 {
-                    year: 2021,
-                    month: "Март",
-                    month_genitive: "Марта",
                     day: 3,
+                    monthNumber: 5,
+                    year: 2021,
                     type: "Online-курс",
                     name: "Свадебный наряд моей дочери",
                     link: "#"
                 },
                 {
-                    month: "Июнь",
-                    month_genitive: "Июня",
                     day: 11,
+                    monthNumber: 6,
+                    year: 2021,
                     type: "Оффлайн-курс",
                     name: "Свадебный наряд для попугая",
                     link: "#"
                 },
                 {
-                    month: "Июль",
-                    month_genitive: "Июля",
                     day: 24,
+                    monthNumber: 7,
+                    year: 2021,
+                    type: "Online-курс",
+                    name: "Свадебный наряд для пингвина",
+                    link: "#"
+                },
+                {
+                    day: 24,
+                    monthNumber: 7,
+                    year: 2021,
                     type: "Online-курс",
                     name: "Свадебный наряд для пингвина",
                     link: "#"
@@ -56,14 +79,61 @@ $(document).ready(function () {
             ],
             selectedEvent: null,
             selectedDay: null,
+            selectedYear: null,
+            selectedMonth: null,
+            firstDayOffset: null
+            // daysInMonth: null,
         },
         methods: {
-            getMonths: function () {
-
-                let arrayOfMonths = this.eventsMonths.find(function (event) {
-                    console.log(event.monthNumber);
+            // getMonths: function () {
+            //     let arrayAllMonths = [];
+            //     let arrayOfMonths = this.events.find(function (event) {
+            //         arrayAllMonths.push({[event.monthNumber]: event.month});
+            //         arrayAllMonths = Array.from(new Set(arrayAllMonths.map(JSON.stringify))).map(JSON.parse);
+            //     });
+            //     return arrayAllMonths;
+            // },
+            // getYear: function () {
+            //     let arrayAllYears = [];
+            //     let arrayOfYears = this.events.find(function (event) {
+            //         arrayAllYears.push([event.year]);
+            //         arrayAllYears = Array.from(new Set(arrayAllYears.map(JSON.stringify))).map(JSON.parse);
+            //     });
+            //     return arrayAllYears;
+            // },
+            // getData: function () {
+            //
+            //     let year = this.getYear();
+            //     let month = this.getMonths();
+            //     this.selectedMonth = month;
+            //     this.selectedYear = year;
+            //
+            // },
+            // getCurrentYear: function (year) {
+            //     return this.months.find(function (event) {
+            //         return event.year == year;
+            //     });
+            // },
+            getCurrentMonth: function () {
+                let arrayAllMonths = [];
+                let arrayNeedMonths = [];
+                let arrayNeedDays = [];
+                let arrayOfMonths = this.months.find(function (event) {
+                    arrayAllMonths.push([{number: event.monthNumber}, {name: event.month}, {days: event.daysInMonth}]);
+                    arrayAllMonths = Array.from(new Set(arrayAllMonths.map(JSON.stringify))).map(JSON.parse);
                 });
-
+                // console.log(arrayAllMonths);
+                // return arrayAllMonths;
+                for (let i = 0; i < arrayAllMonths.length; i++) {
+                    for(let j = 0; j < arrayAllMonths[i].length; j++) {
+                        if (arrayAllMonths[i][j].days) {
+                            arrayNeedDays.push([{day: arrayAllMonths[i][j].days}, {name: arrayAllMonths[i][j].name}]);
+                        }
+                    }
+                }
+                // this.selectedMonth = arrayNeedMonths;
+                console.log(arrayNeedMonths);
+                return arrayNeedMonths;
             },
             getEventByDay: function (day) {
                 return this.events.find(function (event) {
